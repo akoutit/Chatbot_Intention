e03fb4e55fa571f8afc7c41fdb7ea6097cae3278
akoutit <abdellahkoutit@gmail.com>
2025-02-23 12:10:36 +0100

    adding sentencepiece

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,7 +1,7 @@
 # Installer les dépendances nécessaires
 def install_dependencies():
     import subprocess
-    subprocess.call(["pip", "install", "torch", "pandas", "numpy", "transformers", "scikit-learn", "joblib", "nltk"])
+    subprocess.call(["pip", "install", "torch", "pandas", "numpy", "transformers", "scikit-learn", "joblib", "nltk", "sentencepiece"])
     
 
 install_dependencies()
